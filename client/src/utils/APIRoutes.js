576703f437c89409eb68a3d008c758c5c08420ad
mrkeshav.05 <keshavpundhir69@gmail.com>
2025-07-09 10:30:44 +0530

    ..

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host = "http://localhost:8080";
+// export const host = "http://localhost:8080";
+export const host = "https://destecho.onrender.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 // export const logoutRoute = `${host}/api/auth/logout`;
